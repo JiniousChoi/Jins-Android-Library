c7c8c57904a9d72dcb1b163ebf38aeedbbe08f5e
jin choi <jinchoiseoul@gmail.com>
2016-06-27 15:14:08 +0900

    initial commit

---
+++ androidlibrary/src/main/java/com/jinsungchoi/mylibrary/MyAndroidClass.java
@@ -0,0 +1,10 @@
+package com.jinsungchoi.mylibrary;
+
+/**
+ * Created by greenjin on 16. 6. 27.
+ */
+public class MyAndroidClass {
+    public static String printName(String s1, String s2) {
+        return s1 + " " + s2;
+    }
+}
---
+++ javalib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyClass {
+    public static String printName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+}
