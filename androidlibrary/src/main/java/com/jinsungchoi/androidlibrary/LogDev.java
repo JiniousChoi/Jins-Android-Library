517202843d8a972c3ecb5d0161facff889f2b88d
jin choi <jinchoiseoul@gmail.com>
2016-07-21 16:38:29 +0900

    [bugfix] BuildConfig in LogDev is not an interface on which I can implement logics

--- androidlibrary/src/main/java/com/jinsungchoi/androidlibrary/LogDev.java
+++ androidlibrary/src/main/java/com/jinsungchoi/androidlibrary/LogDev.java
@@ -8,32 +8,38 @@ import android.util.Log;
  */
 public class LogDev {
 
+    private static boolean DEBUG = false;
+
+    public static void setDebug(boolean debug) {
+        DEBUG = debug;
+    }
+
     public static void v(String tag, String msg) {
-        if(BuildConfig.DEBUG) {
+        if(DEBUG) {
             Log.v(tag, msg);
         }
     }
 
     public static void d(String tag, String msg) {
-        if(BuildConfig.DEBUG) {
+        if(DEBUG) {
             Log.d(tag, msg);
         }
     }
 
     public static void i(String tag, String msg) {
-        if(BuildConfig.DEBUG) {
+        if(DEBUG) {
             Log.i(tag, msg);
         }
     }
 
     public static void w(String tag, String msg) {
-        if(BuildConfig.DEBUG) {
+        if(DEBUG) {
             Log.w(tag, msg);
         }
     }
 
     public static void e(String tag, String msg) {
-        if(BuildConfig.DEBUG) {
+        if(DEBUG) {
             Log.e(tag, msg);
         }
     }
